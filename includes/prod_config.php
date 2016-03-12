02a839ed005649b7f6f72c403b59ef98b6075a9d
Antoine Boire <antoine.boire@gmail.com>
2016-03-12 14:36:35 +0100

    remove prod_config

--- includes/prod_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("GGBB_DB_NAME", "ggbbdb");
-define('GGBB_DB_USER', 'gifbanger01');
-define('GGBB_DB_PASSWORD', '7mnm9HSEvX49');
-define('GGBB_DB_HOST', 'localhost');
-define('GGBB_DOMAIN', 'camponthemoon.com');
-
-define('GGBB_URL', '//www.gifgifbangbang.com');
-?>
