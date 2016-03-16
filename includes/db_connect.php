233355fc5bacd6f4f473851cadeb4b0af159f3fa
Jérémie Bersani <voldo37@hotmail.com>
2016-03-16 19:02:25 +0100

    change connection to have error messages

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,7 +1,8 @@
 <?php
     try
     {
-      $bdd = new PDO('mysql:host='.GGBB_DB_HOST.';dbname='.GGBB_DB_NAME.';charset=utf8', GGBB_DB_USER, GGBB_DB_PASSWORD);
+    	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+      $bdd = new PDO('mysql:host='.GGBB_DB_HOST.';dbname='.GGBB_DB_NAME.';charset=utf8', GGBB_DB_USER, GGBB_DB_PASSWORD, $pdo_options);
     }
     catch(Exception $e)
     {
