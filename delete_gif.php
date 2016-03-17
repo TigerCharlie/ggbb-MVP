d86253f4ae012658d8a40b5fd8e3891552f80c1e
Antoine Boire <antoine.boire@radiofrance.com>
2016-03-17 16:54:36 +0100

    Refactoring : delete_gif.php

--- delete_gif.php
+++ delete_gif.php
@@ -2,28 +2,21 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 include('includes/config.php');
+include('includes/db_connect.php');
 
-	if( isset($_POST['uuid']) ){
+if (isset($_POST['uuid'])) {
+	$uuid = $_POST['uuid'];
+	$request = 'DELETE FROM shoots WHERE uuid = :uuid';
+	$query = $bdd->prepare($request);
 
-		$uuid = $_POST['uuid'];
-
-	  	include('includes/db_connect.php');
-
-		$req = $bdd->prepare('DELETE FROM shoots WHERE uuid = :uuid');
-
-		if (!$req->execute(array( 'uuid' => $uuid ))) {
-    		echo '{"status_code":-1,"status":"mysql execute error"}';
-		} else{	
-
-			$mask = 'img/'.$uuid.'*.*';
-			array_map('unlink', glob($mask));
-
-			echo '{"status_code":1,"status":"gif deleted !"}';
-		}
-
-	}else{	
-		echo '{"status_code":-1,"status":"the server never received data (uuid)."}';
+	if (!$query->execute(array( 'uuid' => $uuid ))) {
+		echo '{"status_code":-1,"status":"mysql execute error"}';
+	} else {
+		$mask = 'img/'.$uuid.'*.*';
+		array_map('unlink', glob($mask));
+		echo '{"status_code":1,"status":"gif deleted !"}';
 	}
-?>
+} else {
+	echo '{"status_code":-1,"status":"the server never received data (uuid)."}';
+}
