2c02877b19a20aabaeb72fba8f16f6361dd218b0
Antoine Boire <antoine.boire@radiofrance.com>
2016-03-17 16:44:46 +0100

    Refactoring : check_gif.php

--- check_gif.php
+++ check_gif.php
@@ -2,41 +2,29 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 include('includes/config.php');
-
-
-
+include('includes/db_connect.php');
 header('Content-Type: application/json; charset=utf-8');
 
-include('includes/db_connect.php');
-
-if(isset($_POST['uuid'])){
+if (isset($_POST['uuid'])){
 	$uuid = $_POST['uuid'];
-
-	$reponse= $bdd->prepare('SELECT * FROM shoots WHERE uuid = :uuid AND active=1 LIMIT 1');
-	$reponse->execute(array(
-					'uuid' => $uuid
-					));
-
-	if($reponse->rowCount() > 0){
-		//$donnees = $reponse->fetch();
-		while ($donnees = $reponse->fetch())
-		{
+	$request = 'SELECT * FROM shoots WHERE uuid = :uuid AND active=1 LIMIT 1';
+	$response= $bdd->prepare($request);
+	$response->execute(
+		array('uuid' => $uuid)
+	);
+	if ($response->rowCount() > 0) {
+		while ($donnees = $response->fetch()) {
 			$gif_done = $donnees['gif_done'];
-			if($gif_done==1){
+			if ($gif_done == 1) {
 				echo '{"status_code":1,"status":"gif generated !", "gifUrl":"img/'.$uuid.'.gif"}';
-			}else{
+			} else {
 				echo '{"status_code":2,"status":"The gif is not generated yet !"}';
 			}
 		}
-
-	}else{
-		//echo 'le shoot '.$title.' n\'exist pas';
+	} else {
 		echo '{"status_code":0,"status":"the shoot '.$uuid.' doesn\'t exist."}';
 	}
-}else{	
-	//echo 'le titre n\'a pas été transmi';
+} else {
 	echo '{"status_code":-1,"status":"the server never received the uuid."}';
 }
-?>
