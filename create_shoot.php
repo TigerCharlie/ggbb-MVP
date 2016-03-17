fd2dbc2aec8b3c4daeac5848a601f2474b9b8859
Antoine Boire <antoine.boire@radiofrance.com>
2016-03-17 16:52:41 +0100

    Refactoring : create_shoot.php

--- create_shoot.php
+++ create_shoot.php
@@ -2,82 +2,48 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 include('includes/config.php');
 include('includes/db_connect.php');
 
-
-if(isset($_POST['title'])){
+if (isset($_POST['title'])) {
 	$title = $_POST['title'];
 
-	if(isset($_POST['type'])){
+	if (isset($_POST['type'])) {
 		$type = $_POST['type'];
-	}else{
+	} else {
 		$type = 'undefined';
 	}
 
-
 	$uuid = base_convert(time(), 10, 36);
-
 	$frames = 1;
 	$active = 1;
 	$uploaded_frames = 0;
 
-	$q = $bdd->prepare("SELECT title FROM shoots WHERE title = :title AND active=1 LIMIT 1");
-	$q->bindValue(':title', $title);
-	$q->execute();
+	$request = "SELECT title FROM shoots WHERE title = :title AND active=1 LIMIT 1";
+	$query = $bdd->prepare($request);
+	$query->bindValue(':title', $title);
+	$query->execute();
 
 
-	if($q->rowCount() > 0){
-
-		//echo 'le titre '.$title.' exist déjà'.mysql_num_rows($reponse);
+	if ($query->rowCount() > 0) {
 		echo '{"status_code":0,"status":"The shoot named : '.$title.' already exist. Please choose another title or join the shoot."}';
+	} else {
+		$insertRequest = 'INSERT INTO shoots(uuid, type, title, frames, active, uploaded_frames) VALUES(:uuid, :type, :title, :frames, :active, :uploaded_frames)';
+		$insertQuery = $bdd->prepare($insertRequest);
 
-	}else{
-
-		$req = $bdd->prepare('INSERT INTO shoots(uuid, type, title, frames, active, uploaded_frames) VALUES(:uuid, :type, :title, :frames, :active, :uploaded_frames)');
-		/*$req->execute(array(
-		'uuid' => $uuid,
-		'type' => $type,
-		'title' => $title,
-		'frames' => $frames,
-		'active' => $active,
-		'uploaded_frames' => $uploaded_frames
-		));*/
-		
-		function escapeJsonString($value) {
-		    # list from www.json.org: (\b backspace, \f formfeed)    
-		    $escapers =     array("\\",     "/",   "\"",  "\n",  "\r",  "\t", "\x08", "\x0c");
-		    $replacements = array("\\\\", "\\/", "\\\"", "\\n", "\\r", "\\t",  "\\f",  "\\b");
-		    $result = str_replace($escapers, $replacements, $value);
-		    return $result;
-		  }
-
-
-
-		if (!$req->execute(array(
-		'uuid' => $uuid,
-		'type' => $type,
-		'title' => $title,
-		'frames' => $frames,
-		'active' => $active,
-		'uploaded_frames' => $uploaded_frames
+		if (!$insertQuery->execute(array(
+			'uuid' => $uuid,
+			'type' => $type,
+			'title' => $title,
+			'frames' => $frames,
+			'active' => $active,
+			'uploaded_frames' => $uploaded_frames
 		))) {
-    		//echo '{"status_code":-1,"status":"mysql execute error"}';
-    		
     		echo mysql_error();
-
-    		//die(mysql_error());
-		} else{	
-
+		} else {
 			echo '{"status_code":1,"status":"Le shoot named : '.$title.' is created.", "title":"'.$title.'", "userFrame":1 ,"uuid":"'.$uuid.'"}';
 		}
-
-
-		//echo 'Le shoot '.$title.' a bien été créé'.$uuid.' ::'.$q->rowCount();
-		
 	}
-}else{	
+} else {
 	echo '{"status_code":-1,"status":"the server never reveved the title."}';
 }
-?>
