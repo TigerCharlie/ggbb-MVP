2001d568ad967ad93de58122802a67561d0fb96c
Antoine Boire <antoine.boire@radiofrance.com>
2016-03-17 17:45:48 +0100

    Refactoring : leave_shoot.php

--- leave_shoot.php
+++ leave_shoot.php
@@ -2,57 +2,38 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 include('includes/config.php');
-
 include('includes/db_connect.php');
 
-
-if(isset($_POST['uuid'])){
+if (isset($_POST['uuid'])) {
 	$uuid = $_POST['uuid'];
-
-	$reponse= $bdd->prepare('SELECT * FROM shoots WHERE uuid = :uuid AND active=1 LIMIT 1');
-	$reponse->execute(array(
-					'uuid' => $uuid
-					));
-
-
-	/*$reponse = $bdd->query('SELECT * FROM shoots WHERE title=$title');*/
-
-
-	if($reponse->rowCount() > 0){
-		//$donnees = $reponse->fetch();
-
-		while ($donnees = $reponse->fetch())
-		{	
-
+	$query = 'SELECT * FROM shoots WHERE uuid = :uuid AND active=1 LIMIT 1';
+	$response = $bdd->prepare($query);
+	$response->execute(
+		array('uuid' => $uuid)
+	);
+	if ($reponse->rowCount() > 0) {
+		while ($donnees = $reponse->fetch()) {
 			$uuid = $donnees['uuid'];
 			$frames = $donnees['frames']-1;
-
-			if($frames == 0){
+			if ($frames == 0) {
 				$active = 0;
-			}else{
+			} else {
 				$active = 1;
 			}
-
-
 			$req = $bdd->prepare('UPDATE shoots SET frames = :frames, active = :active WHERE uuid = :uuid');
-			$req->execute(array(
+			$req->execute(
+				array(
 					'uuid' => $uuid,
 					'frames' => $frames,
 					'active' => $active
-					));
-			
+				)
+			);
 			echo '{"status_code":1,"status":"You well leaved the shoot named : \"'.$donnees['title'].'\". Bye bye !"}';
 		}
-
-	}else{
-
-		//echo 'le shoot '.$title.' n\'exist pas';
+	} else {
 		echo '{"status_code":0,"status":"the shoot named \"'.$title.'\" doesn\'t exist.","title":"'.$title.'"}';
 	}
-}else{	
-	//echo 'le titre n\'a pas été transmi';
+} else {
 	echo '{"status_code":-1,"status":"the server never reveved the uuid."}';
 }
-?>
