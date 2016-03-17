9579669cc5c03d144bde591395bcccb811fc7cf0
Antoine Boire <antoine.boire@radiofrance.com>
2016-03-17 17:54:41 +0100

    Refactoring : timer.php

--- timer.php
+++ timer.php
@@ -2,41 +2,30 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
 include('includes/config.php');
-
+include('includes/db_connect.php');
 header('Content-Type: application/json; charset=utf-8');
 
-include('includes/db_connect.php');
-
-if(isset($_POST['uuid'])){
+if (isset($_POST['uuid'])){
 	$uuid = $_POST['uuid'];
-
-	$reponse= $bdd->prepare('SELECT * FROM shoots WHERE uuid = :uuid AND active=1 LIMIT 1');
-	$reponse->execute(array(
-					'uuid' => $uuid
-					));
-
-	if($reponse->rowCount() > 0){
-		//$donnees = $reponse->fetch();
-		while ($donnees = $reponse->fetch())
-		{
+	$response = $bdd->prepare('SELECT * FROM shoots WHERE uuid = :uuid AND active=1 LIMIT 1');
+	$response->execute(
+		array('uuid' => $uuid)
+	);
+	
+	if ($response->rowCount() > 0){
+		while ($donnees = $response->fetch()) {
 			$serverTimestamp = round(microtime(true)*1000);
 			$shootTimestamp = strtotime($donnees['shoottime']);
-			
-			if(!$shootTimestamp){
+			if (!$shootTimestamp){
 				echo '{"serverTimestamp":'.$serverTimestamp.',"status_code":1,"status":"The shoot : \"'.$donnees['title'].'\". is not programmed yet.","title":"'.$donnees['title'].'","frames":'.$donnees['frames'].'}';
-			}else{
+			} else {
 				echo '{"serverTimestamp":'.$serverTimestamp.', "shootTime":'.$shootTimestamp.',"status_code":2,"status":"The shoot : \"'.$donnees['title'].'\". is programmed very soon.","title":"'.$donnees['title'].'","frames":'.$donnees['frames'].'}';
 			}
 		}
-
-	}else{
-		//echo 'le shoot '.$title.' n\'exist pas';
+	} else {
 		echo '{"status_code":0,"status":"the shoot '.$uuid.' doesn\'t exist."}';
 	}
-}else{	
-	//echo 'le titre n\'a pas été transmi';
+} else {
 	echo '{"status_code":-1,"status":"the server never received the uuid."}';
 }
-?>
